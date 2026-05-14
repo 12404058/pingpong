bc4a12774810c4f39e8783543761fb83f246f2e9
12404058 <garkushinelisey@gmail.com>
2026-05-14 19:24:16 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -24,9 +24,9 @@ class Player(GameSprite):
 
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 0:
+        if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 300:
+        if keys[K_DOWN] and self.rect.y < 300:
             self.rect.y += self.speed
 
 
